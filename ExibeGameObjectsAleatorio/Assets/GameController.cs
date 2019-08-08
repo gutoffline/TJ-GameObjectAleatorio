5433d01a43b8ef52ed6e63ee9fe8a3379a8ac44d
Guto Xavier <gutoffline@gmail.com>
2019-08-07 21:26:18 -0300

    Game Objects aleatorios

---
+++ ExibeGameObjectsAleatorio/Assets/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] listaDeBolas;
+    private int bolaAtual = 0;
+    float tempoPassado = 0f;
+    float tempoTroca = 5f; 
+    
+    public void NovoObjetoAleatorio()
+     {
+         int novaBola = Random.Range(0, listaDeBolas.Length);
+         listaDeBolas[bolaAtual].SetActive(false);
+         bolaAtual = novaBola;
+         listaDeBolas[bolaAtual].SetActive(true);
+     }
+
+    void Update()
+    {
+        tempoPassado += Time.deltaTime;
+        if (tempoPassado >= tempoTroca)
+        {
+            NovoObjetoAleatorio();
+            tempoPassado -= tempoTroca;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# TJ-GameObjectAleatorio
-Exibe aleatoriamente um game object
+# Exibindo GameObjects Aleatoriamente
+
+![GameDev](icones/game1.png)
+![GameDev](icones/unity1.png)
+![GameDev](icones/git.png)
+![GameDev](icones/csharp.png)
+
+___
+
+Exemplo de como exibir GameObjects de forma aleatório a cada 10 (ou 5) segundos.
+
+
+
